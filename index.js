7fdc13be31686022f8a84209d7c2b7ca6be9f046
Desmonne Dotson <brazette97@gmail.com>
2024-02-03 14:09:09 -0600

    array filter

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Code your solution here
+function findMatching(drivers, string) {
+   const results = drivers.filter((driver) => driver.toLowerCase() === string.toLowerCase())
+   return results;
+}
+//console.log(findMatching(drivers, 'Susan'));
+
+function fuzzyMatch(drivers, string) {
+    return drivers.filter((driver) => driver.startsWith(string));
+}
+
+const drivers = [
+    {
+      name: 'Bobby',
+      hometown: 'Pittsburgh' },
+    {
+      name: 'Sammy',
+      hometown: 'New York' } ,
+    {
+      name: 'Sally',
+      hometown: 'Cleveland' },
+    {
+      name: 'Annette',
+      hometown: 'Los Angeles' },
+    {
+      name: 'Bobby',
+      hometown: 'Tampa Bay' }
+  ];
+
+function matchName(drivers, string) {
+     return drivers.filter((drivers) => drivers.name === string);
+}
+console.log(matchName(drivers, 'Annette'));
